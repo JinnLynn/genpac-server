43923454ef4193a76037cee7ff3e2133c68e71c5
JinnLynn <eatfishlin@gmail.com>
2018-11-08 16:32:45 +0800

    add sample

--- setup.py
+++ setup.py
@@ -6,12 +6,10 @@ Web server for GenPAC.
 For more information, please visit `project page`_.
 
 
-.. |pypi version| image:: https://img.shields.io/pypi/v/genpac.svg
-   :target: https://pypi.python.org/pypi/genpac-server
-.. |pypi license| image:: https://img.shields.io/pypi/l/genpac.svg
-   :target: https://pypi.python.org/pypi/genpac
-.. |travis ci| image:: https://img.shields.io/travis/JinnLynn/genpac.svg
-   :target: https://travis-ci.org/JinnLynn/genpac
+.. |pypi version| image:: https://img.shields.io/pypi/v/genpac-server.svg
+   :target: https://pypi.org/project/genpac-server/
+.. |pypi license| image:: https://img.shields.io/pypi/l/genpac-server.svg
+   :target: https://pypi.org/project/genpac-server/
 
 .. _project page: https://github.com/JinnLynn/genpac-server/
 '''
